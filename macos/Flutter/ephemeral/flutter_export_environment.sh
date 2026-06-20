3ecf9c2a036e0d48e31df027bc22a141042c420a
tony <tony@gmail.com>
2026-06-20 18:48:27 +0700

    更改logo

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ma/data/flutter3.51"
+export "FLUTTER_ROOT=/Users/ma/data/flutter3.22.3"
 export "FLUTTER_APPLICATION_PATH=/Users/ma/data/code/openim/pppim-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.8"
+export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
