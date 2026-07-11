65f0f9699c89d1c6551a3e0aa108fcd6a7509205
tony <tony@qq.com>
2026-07-12 02:12:01 +0800

    增加好友人数，隐藏群撤回消息提示

--- lib/pages/contacts/select_contacts/friend_list/friend_list_view.dart
+++ lib/pages/contacts/select_contacts/friend_list/friend_list_view.dart
@@ -14,55 +14,59 @@ class SelectContactsFromFriendsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TitleBar.back(title: StrRes.myFriend),
-      backgroundColor: Styles.c_F8F9FA,
-      body: Column(
-        children: [
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: logic.searchFriend,
-            child: Container(
-              color: Styles.c_FFFFFF,
-              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
-              child: const SearchBox(),
-            ),
-          ),
-          if (selectContactsLogic.isMultiModel)
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.h),
-              child: Ink(
-                height: 64.h,
+    return Obx(
+      () => Scaffold(
+        appBar: TitleBar.back(
+          title: '${StrRes.myFriend}（${logic.friendList.length}）',
+        ),
+        backgroundColor: Styles.c_F8F9FA,
+        body: Column(
+          children: [
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: logic.searchFriend,
+              child: Container(
                 color: Styles.c_FFFFFF,
-                child: InkWell(
-                  onTap: logic.selectAll,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 16.w),
-                    child: Row(
-                      children: [
-                        Obx(() => Padding(
-                              padding: EdgeInsets.only(right: 10.w),
-                              child: ChatRadio(checked: logic.isSelectAll),
-                            )),
-                        10.horizontalSpace,
-                        StrRes.selectAll.toText..style = Styles.ts_0C1C33_17sp,
-                      ],
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
+                child: const SearchBox(),
+              ),
+            ),
+            if (selectContactsLogic.isMultiModel)
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: Ink(
+                  height: 64.h,
+                  color: Styles.c_FFFFFF,
+                  child: InkWell(
+                    onTap: logic.selectAll,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      child: Row(
+                        children: [
+                          Obx(() => Padding(
+                                padding: EdgeInsets.only(right: 10.w),
+                                child: ChatRadio(checked: logic.isSelectAll),
+                              )),
+                          10.horizontalSpace,
+                          StrRes.selectAll.toText..style = Styles.ts_0C1C33_17sp,
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          Flexible(
-            child: Obx(
-              () => WrapAzListView<ISUserInfo>(
-                data: logic.friendList,
-                itemCount: logic.friendList.length,
-                itemBuilder: (_, data, index) => _buildItemView(data),
+            Flexible(
+              child: Obx(
+                () => WrapAzListView<ISUserInfo>(
+                  data: logic.friendList,
+                  itemCount: logic.friendList.length,
+                  itemBuilder: (_, data, index) => _buildItemView(data),
+                ),
               ),
             ),
-          ),
-          selectContactsLogic.checkedConfirmView,
-        ],
+            selectContactsLogic.checkedConfirmView,
+          ],
+        ),
       ),
     );
   }
