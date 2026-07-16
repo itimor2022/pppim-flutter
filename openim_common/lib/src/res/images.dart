c855e3f90253ef752f568a47b3d4a87cf579fb17
tony <tony@qq.com>
2026-07-16 18:11:43 +0800

    fff

--- openim_common/lib/src/res/images.dart
+++ openim_common/lib/src/res/images.dart
@@ -14,7 +14,7 @@ class ImageRes {
   static const voiceBlueAnim = "assets/anim/voice_blue.json";
   static const voiceBlackAnim = "assets/anim/voice_black.json";
 
-  static const splashLogo = "$_dir/app-icon.webp";
+  static const splashLogo = "$_dir/ic_splash_logo.webp";
   static const loginLogo = "$_dir/ic_login_logo.webp";
   static const eyeOpen = "$_dir/ic_eye_open.webp";
   static const eyeClose = "$_dir/ic_eye_close.webp";
